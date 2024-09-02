2c1936b7074be9983bac9769c8960da1d6d73e63
DanStew <danielstewarts2004@gmail.com>
2024-09-02 13:45:58 +0100

    Making the Photos screen, and the Photo Slideshow

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,35 @@
 import BottomBanner from "./BottomBanner"
 
 function Home(){
+
+    function goToSection(location){
+        const aTag = document.createElement("a")
+        aTag.href = "#" + location
+        document.body.appendChild(aTag)
+        aTag.click()
+        aTag.remove()
+    }
+
     return(
         <div className="screen flexItems">
             <div className="home mainBody oneBannerBody flexItems">
-                Home
+                <div className="mainTitle">
+                    <p>Football Manager</p>
+                    <p>Player Analyser</p>
+                </div>
+                <div className="information">
+                    <p>This is a Football Manager Player Analyser Application made using Python. It includes a CLI and GUI interface for the user to use and enables you to be able to generate Player Scores for players that you input into the system. The system also enable you to change the attribute rankings, to customise the player scores for your need. More infromation on how to use the system can be seen within the download folder.</p>
+                </div>
+                <div className="transportArea">
+                    <div className="subheading">
+                        <p className="flexItems">Jump To : </p>
+                    </div>
+                    <div className="buttonsArea flexItems">
+                        <button onClick={() => goToSection("Photos")}>Photos</button>
+                        <button onClick={() => goToSection("Download")}>Download</button>
+                        <button onClick={() => goToSection("Information")}>Information</button>
+                    </div>
+                </div>
             </div>
             <BottomBanner screenName="Photos"/>
         </div>
--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -1,12 +1,55 @@
 import BottomBanner from "./BottomBanner"
 import TopBanner from "./TopBanner"
 
+//Importing the images
+import GUIHomeScreen from "../Images/FMGUIHome.png"
+import GUIPlayerScores from "../Images/FMGUIPlayerScores.png"
+import GUIOutputTable from "../Images/FMGUIOutputTable.png"
+import GUIUpdateAttributes from "../Images/FMGUIUpdateRankings.png"
+
 function Photos(){
+
+    //Function to automatically navigate the windows
+    var counter = 1
+    setInterval(function(){
+        document.getElementById("radio" + counter).checked = true
+        counter++
+        if (counter > 4){
+            counter = 1
+        }
+    },5000)
+
     return(
         <div className="screen flexItems">
             <TopBanner screenName="Home"/>
             <div className="photos mainBody twoBannerBody flexItems">
-                Photos
+                <div className="information">
+                    <p>Below can be seen some of the images from the GUI section of this application. This includes the HTML Output Table that will be produced when making player scores.</p>
+                </div>
+                <div className="slideshow">
+                    <div className="slider">
+                        <div className="slides">
+                            {/* Making the radio buttons to use */}
+                            <input type="radio" name="radio-btn" id="radio1"/>
+                            <input type="radio" name="radio-btn" id="radio2"/>
+                            <input type="radio" name="radio-btn" id="radio3"/>
+                            <input type="radio" name="radio-btn" id="radio4"/>
+                            {/* Making the slides */}
+                            <div className="slide first">
+                                <img src={GUIHomeScreen} alt="GUI Home Screen" />
+                            </div>
+                            <div className="slide">
+                                <img src={GUIPlayerScores} alt="GUI Player Scores Screen" />
+                            </div>
+                            <div className="slide">
+                                <img src={GUIOutputTable} alt="GUI Player Scores Output Table" />
+                            </div>
+                            <div className="slide">
+                                <img src={GUIUpdateAttributes} alt="GUI Update Attribute Rankings Screen" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <BottomBanner screenName="Download"/>
         </div>
